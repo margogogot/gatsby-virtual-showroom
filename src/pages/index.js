991032e134f51bbbb350692655de864fbe59b66b
Michael Wieczorkowski <mwieczorkowski@gmail.com>
2020-10-28 14:40:37 -0400

    updates content

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,13 +18,17 @@ import perk3Img from "../images/friendly-staff.svg"
 
 import heroImage1 from "../images/hero-images/5.2.20_factor_chair_settings_Factor.457_09242020.jpg"
 import heroImage2 from "../images/hero-images/5.2.20_factor_table_studio_Factor.TA1_.408_09242020.jpg"
-import factorChair1 from '../images/hero-images/factor-chair.png'
+import factorChair from '../images/hero-images/factor-chair.png'
 import factorChair2 from '../images/hero-images/factor-chair-2.png'
 import factorChair3 from '../images/hero-images/factor-chair-3.png'
 import aptosPedestrian1 from '../images/hero-images/aptos-pedestrian-1.png'
 import aptosPedestrian2 from '../images/hero-images/aptos-pedestrian-2.png'
-import aptosBollard from '../images/hero-images/aptos-bollard-1.png'
-import radiaBollard from '../images/hero-images/aptos-bollard-1.png'
+import aptosBollard from '../images/hero-images/aptos-bollard-2.png'
+import radiaBollard from '../images/hero-images/radia-bollard-5.png'
+import helioBollard from '../images/hero-images/helio-bollard-1.png'
+import tonyoReceptacle from '../images/hero-images/tonyo-receptacle-1.png'
+import factorTable from '../images/hero-images/factor-table-1.png'
+import liniaChair from '../images/hero-images/linia-cafe-1.png'
 import { IconContext } from "react-icons"
 import { MdDone, MdClear } from "react-icons/md"
 
@@ -37,7 +41,7 @@ export default () => (
         label="APTOS PEDESTRIAN LIGHTING"
         heroId="aptos-pedestrian"
         >
-          <div bigImage={aptosPedestrian1} bgColor='#999999'>
+          <div bigImage={aptosPedestrian1} bgColor='#918f8a'>
             <p>Aptos Pedestrian Lighting blends graceful lines, performance LEDs, and the visual lightness and distinctive design of the coordinating Aptos Bollard.</p>
             <h2 className='hero-title'>APTOS PEDESTRIAN LIGHTING</h2>
             <div className='heroTextInner'>
@@ -60,7 +64,7 @@ export default () => (
       label="APTOS BOLLARD"
       heroId="aptos-bollard"
       >
-        <div bigImage={aptosBollard} bgColor='#996666'>
+        <div bigImage={aptosBollard} bgColor='#877f76'>
           <p>Aptos Bollards unite the best of two worlds with solar-powered and hardwired designs and coordinate beautifully with Aptos Pedestrian Lighting.</p>
           <h2 className='hero-title'>APTOS BOLLARD</h2>
           <div className='heroTextInner'>
@@ -103,103 +107,118 @@ export default () => (
           <p><a href="#contact">KEEP ME POSTED</a></p>
         </div>
       </div>
-        <div
-        label="Factor Chair"
-        bigImage={heroImage1}
-        heroId="factor-chair"
-        >
-          <div bigImage={factorChair1} bgColor='#8c4131'>
-            <p>Highly configurable Factor Chairs coordinate with Factor Tables and unite clean lines, simple geometry, and go-anywhere versatility. </p>
-            <h2 className='hero-title'>Factor Chair</h2>
-            <div className='heroTextInner'>
-              <ul>
-                <li>Tubular stainless steel frames with a durable powdercoat finish</li>
-                <li>Seats and backs of powdercoated formed aluminum with or without perforations; or seats of FSC 100% Cumaru or FSC 100% Teak hardwood slats with backs of powdercoated cast aluminum</li>
-                <li>Optional armrests</li>
-                <li>Generally stackable to four high</li>
-                <li>Chairs with and without arms can be stacked together</li>
-                <li>Freestanding; black nylon glides on chair feet</li>
-                <li>High recycled content; highly recyclable</li>
-                <li>Suitable for interior and exterior applications</li>
-              </ul>
-            </div>
-            <p><a href="#contact-us">MORE INFO</a></p>
+      <div
+      label="HELIO SECURITY BOLLARD"
+      heroId="helio-bollard"
+      >
+        <div bigImage={helioBollard} bgColor='#c5c1be'>
+          <p>Helio Security Bollards offer robust protection for public spaces of all kinds with M30-P1/K4, M40-P1/K8 and M50-P1/K12 impact ratings.</p>
+          <h2 className='hero-title'>HELIO BOLLARD</h2>
+          <div className='heroTextInner'>
+            <ul>
+              <li>Stainless steel construction</li>
+              <li>Satin finish; durable powdercoat finish also available</li>
+              <li>Column diameter is 9.25" for M30/K4; 11.5" for M40/K8 and M50/K12</li>
+              <li>Custom LED light engine with Cree® LEDs in 3000K warm white and 4000K neutral white</li>
+              <li>180° or 360° light distribution options</li>
+              <li>Non-illuminated options available</li>
+              <li>ETL and C-ETL listed</li>
+              <li>High recycled content; highly recyclable</li>
+              <li>Coordinating 6" diameter Helio Bollards in illuminated, non-illuminated, and medium security core variations also available</li>
+            </ul>
           </div>
-          <div bigImage={factorChair2} bgColor='#434a3e'>
-            <p>Highly configurable Factor Chairs coordinate with Factor Tables and unite clean lines, simple geometry, and go-anywhere versatility. </p>
-            <h2 className='hero-title'>Factor Chair</h2>
-            <div className='heroTextInner'>
-              <ul>
-                <li>Tubular stainless steel frames with a durable powdercoat finish</li>
-                <li>Seats and backs of powdercoated formed aluminum with or without perforations; or seats of FSC 100% Cumaru or FSC 100% Teak hardwood slats with backs of powdercoated cast aluminum</li>
-                <li>Optional armrests</li>
-                <li>Generally stackable to four high</li>
-                <li>Chairs with and without arms can be stacked together</li>
-                <li>Freestanding; black nylon glides on chair feet</li>
-                <li>High recycled content; highly recyclable</li>
-                <li>Suitable for interior and exterior applications</li>
-              </ul>
-            </div>
-            <p><a href="#">MORE INFO</a></p>
-          </div>
-          <div bigImage={factorChair3} bgColor="#464649">
-            <p>Highly configurable Factor Chairs coordinate with Factor Tables and unite clean lines, simple geometry, and go-anywhere versatility. </p>
-            <h2 className='hero-title'>Factor Chair</h2>
-            <div className='heroTextInner'>
-              <ul>
-                <li>Tubular stainless steel frames with a durable powdercoat finish</li>
-                <li>Seats and backs of powdercoated formed aluminum with or without perforations; or seats of FSC 100% Cumaru or FSC 100% Teak hardwood slats with backs of powdercoated cast aluminum</li>
-                <li>Optional armrests</li>
-                <li>Generally stackable to four high</li>
-                <li>Chairs with and without arms can be stacked together</li>
-                <li>Freestanding; black nylon glides on chair feet</li>
-                <li>High recycled content; highly recyclable</li>
-                <li>Suitable for interior and exterior applications</li>
-              </ul>
-            </div>
-            <p><a href="#">MORE INFO</a></p>
-          </div>
+          <p><a href="#contact">MORE INFO</a></p>
         </div>
-        <div
-        label="Aptos Pedestrian"
-        bigImage={aptosPedestrian1}
-        heroId="aptos-pedestrian"
-        >
-          <div bigImage={aptosPedestrian1} bgColor='#918f8a'>
-            <p>Highly configurable Factor Chairs coordinate with Factor Tables and unite clean lines, simple geometry, and go-anywhere versatility. </p>
-            <h2 className='hero-title'>Aptos Pedestrian</h2>
-            <div className='heroTextInner'>
-              <ul>
-                <li>Tubular stainless steel frames with a durable powdercoat finish</li>
-                <li>Seats and backs of powdercoated formed aluminum with or without perforations; or seats of FSC 100% Cumaru or FSC 100% Teak hardwood slats with backs of powdercoated cast aluminum</li>
-                <li>Optional armrests</li>
-                <li>Generally stackable to four high</li>
-                <li>Chairs with and without arms can be stacked together</li>
-                <li>Freestanding; black nylon glides on chair feet</li>
-                <li>High recycled content; highly recyclable</li>
-                <li>Suitable for interior and exterior applications</li>
-              </ul>
-            </div>
-            <p><a href="#">MORE INFO</a></p>
+      </div>
+      <div
+      label="TONYO RECEPTACLES"
+      heroId="tonyo-receptacle"
+      >
+        <div bigImage={tonyoReceptacle} bgColor='#626d6f'>
+          <p>Tonyo Litter & Recycling Receptacles are durable, highly configurable, and offer impressive design, size, material, and indoor/outdoor flexibility.</p>
+          <h2 className='hero-title'>TONYO RECEPTACLES</h2>
+          <div className='heroTextInner'>
+            <ul>
+              <li>Stainless steel frame</li>
+              <li>Hinged side-opening door with slam latch</li>
+              <li>Doors in stainless steel or with FSC® 100% Cumaru hardwood slats</li>
+              <li>Durable powdercoat finish</li>
+              <li>30G,36G and 60-gallon capacities available</li>
+              <li>Single and dual waste stream options with corresponding liners</li>
+              <li>Configurations with and without rain cover</li>
+              <li>Freestanding, surface mount, or pole mount (30G)</li>
+              <li>Suitable for interior and exterior applications</li>
+              <li>High recycled content; fully recyclable</li>
+              <li>Launching in early 2021</li>
+            </ul>
           </div>
-          <div bigImage={aptosPedestrian2} bgColor='#669999'>
-            <p>Highly configurable Factor Chairs coordinate with Factor Tables and unite clean lines, simple geometry, and go-anywhere versatility. </p>
-            <h2 className='hero-title'>Aptos Pedestrian</h2>
-            <div className='heroTextInner'>
-              <ul>
-                <li>Tubular stainless steel frames with a durable powdercoat finish</li>
-                <li>Seats and backs of powdercoated formed aluminum with or without perforations; or seats of FSC 100% Cumaru or FSC 100% Teak hardwood slats with backs of powdercoated cast aluminum</li>
-                <li>Optional armrests</li>
-                <li>Generally stackable to four high</li>
-                <li>Chairs with and without arms can be stacked together</li>
-                <li>Freestanding; black nylon glides on chair feet</li>
-                <li>High recycled content; highly recyclable</li>
-                <li>Suitable for interior and exterior applications</li>
-              </ul>
-            </div>
-            <p><a href="#">MORE INFO</a></p>
-          </div>
+          <p><a href="#contact">KEEP ME POSTED</a></p>
         </div>
+      </div>
+      <div
+      label="FACTOR TABLE"
+      heroId="factor-table"
+      >
+        <div bigImage={factorTable} bgColor='#434a3e'>
+          <p>Highly configurable Factor Tables coordinate with Factor Chairs and unite clean lines, simple geometry, and go-anywhere versatility.</p>
+          <h2 className='hero-title'>FACTOR TABLE</h2>
+          <div className='heroTextInner'>
+            <ul>
+              <li>Stainless steel frames with a durable powdercoat finish</li>
+              <li>Tops of FSC® 100% Cumaru or FSC® 100% Teak hardwood slats or powdercoated aluminum with or without perforations</li>
+              <li>Standard table size is 42" square</li>
+              <li>Freestanding with adjustable levelers or surface mount</li>
+              <li>Optional umbrella hole</li>
+              <li>Highly recyclable</li>
+              <li>Suitable for interior and exterior applications</li>
+            </ul>
+          </div>
+          <p><a href="#contact">MORE INFO</a></p>
+        </div>
+      </div>
+      <div
+      label="FACTOR CHAIR"
+      heroId="factor-chair"
+      >
+        <div bigImage={factorChair} bgColor='#8c4131'>
+          <p>Highly configurable Factor Chairs coordinate with Factor Tables and unite clean lines, simple geometry, and go-anywhere versatility.</p>
+          <h2 className='hero-title'>FACTOR CHAIR</h2>
+          <div className='heroTextInner'>
+            <ul>
+              <li>Tubular <u>stainless steel</u> frames with a durable powdercoat finish</li>
+              <li>Seats and backs of powdercoated formed aluminum with or without perforations; or seats of FSC® 100% Cumaru or FSC® 100% Teak hardwood slats with backs of powdercoated cast aluminum</li>
+              <li>Optional armrests</li>
+              <li>Generally stackable to four high</li>
+              <li>Chairs with and without arms can be stacked together</li>
+              <li>Freestanding; black nylon glides on chair feet</li>
+              <li>High recycled content; highly recyclable</li>
+              <li>Suitable for interior and exterior applications</li>
+            </ul>
+          </div>
+          <p><a href="#contact">MORE INFO</a></p>
+        </div>
+      </div>
+      <div
+      label="LINIA CHAIR"
+      heroId="linia-chair"
+      >
+        <div bigImage={liniaChair} bgColor='#c8c4b3'>
+          <p>Linia is a versatile, durable, moveable chair defined by crisp lines, clean geometric forms, and the option to play with material finish combinations.</p>
+          <h2 className='hero-title'>LINIA CHAIR</h2>
+          <div className='heroTextInner'>
+            <ul>
+              <li>Solid stainless-steel rod frames with a polished or powdercoat finish</li>
+              <li>Seats and backs of powdercoated formed stainless steel with or without perforations</li>
+              <li>Generally stackable to four high; black nylon stacking bumpers protect seats and backs</li>
+              <li>Freestanding; black nylon glides on chair feet</li>
+              <li>High recycled content; highly recyclable</li>
+              <li>Suitable for interior and exterior applications</li>
+              <li>Launching in early 2021</li>
+            </ul>
+          </div>
+          <p><a href="#contact">KEEP ME POSTED</a></p>
+        </div>
+      </div>
       </Heroes>
       <Contact
         id="contact"
