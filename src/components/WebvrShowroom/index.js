a77d1877d1c7a232a291bddb77d8bdfe0857c223
Michael Wieczorkowski <mwieczorkowski@gmail.com>
2021-10-26 10:23:33 -0400

    adjust iframe and overlay height

--- src/components/WebvrShowroom/index.js
+++ src/components/WebvrShowroom/index.js
@@ -160,7 +160,7 @@ const BannerWrapper = styled.section`
   position: relative;
   .iframe-wrapper {
     width: 100%;
-    height: calc(100vh - 72px);
+    height: calc(100vh - 70px);
     max-height: 100%;
     /* min-height: 100%; */
     border: none;
@@ -188,7 +188,7 @@ const BannerWrapper = styled.section`
     top: 0px;
     left: 0px;
     width: 100%;
-    height: 100%;
+    height: calc(100vh - 70px);
     z-index:3;
     background-image: url(${virtualBoothBGMobile});
     background-position: center center;
